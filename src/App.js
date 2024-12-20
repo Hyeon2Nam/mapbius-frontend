32a8c87694ae005595f1864f7be10b55f2dc4cda
nhy <gusdlnam@gmail.com>
2024-12-20 10:49:27 +0900

    add temp main page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import Login from "./login/Login";
 import Register from "./login/Register";
 import Auth from "./login/Auth";
 import SelectRegisterWay from "./login/SelectRegisterWay";
+import MainPage from "./map/Temp";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/select-register" element={<SelectRegisterWay />} />
         <Route path="/default-register" element={<Register />} />
         <Route path="/kakao-register" element={<Register />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
-
-      <Link to={"/login"}>Login</Link>
-      <br />
-      <Link to={"/select-register"}>register</Link>
     </div>
   );
 }
