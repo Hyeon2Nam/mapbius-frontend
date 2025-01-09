5724aac927b1b826bf7b4e0e9b277dca5f6e56a8
nhy <gusdlnam@gmail.com>
2025-01-09 18:19:33 +0900

    feat : add photo file without func

--- src/mypage/ProfileImageUpload.js
+++ src/mypage/ProfileImageUpload.js
@@ -25,44 +25,37 @@ const ProfileImageUpload = () => {
     const formData = new FormData();
     formData.append("file", file); // 업로드할 파일
 
-    // uploadProfileImg(formData, localStorage.getItem("userToken"))
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((e) => {
-    //     console.log(e);
-    //   });
+    uploadProfileImg(formData, localStorage.getItem("userToken"))
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
 
-    try {
-      console.log(formData instanceof FormData);
+    // try {
+    //   console.log(formData instanceof FormData);
 
-      await axios.post(
-        "http://127.0.0.1:8080/api/private/account/uploadProfileImage",
-        formData,
-        {
-          headers: {
-            // 직접 "Content-Type": "multipart/form-data" 지정하지 않기!
-            Authorization: "Bearer " + localStorage.getItem("userToken"),
-          },
-        }
-      );
-      alert("업로드 성공!");
-    } catch (error) {
-      console.error("업로드 실패:", error);
-      alert("업로드 중 오류가 발생했습니다.");
-    }
+    //   await axios.post(
+    //     "http://127.0.0.1:8080/api/private/account/uploadProfileImage",
+    //     formData,
+    //     {
+    //       headers: {
+    //         "Content-Type": "multipart/form-data",
+    //         Authorization: "Bearer " + localStorage.getItem("userToken"),
+    //       },
+    //     }
+    //   );
+    //   alert("업로드 성공!");
+    // } catch (error) {
+    //   console.error("업로드 실패:", error);
+    //   alert("업로드 중 오류가 발생했습니다.");
+    // }
   };
 
   return (
     <div>
-      <h1>프로필 이미지 업로드</h1>
       <input type="file" accept="image/*" onChange={handleFileChange} />
-      {preview && (
-        <div>
-          <h2>미리보기</h2>
-          <img src={preview} alt="이미지 미리보기" width="200" />
-        </div>
-      )}
       <button onClick={handleUpload}>업로드</button>
     </div>
   );
