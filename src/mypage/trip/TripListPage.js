15e96ea7ebf55890f6c2182d99c7678d409e9ee8
NHY <gusdlnam@gmail.com>
2025-06-08 19:55:13 +0900

    add dump data

--- src/mypage/trip/TripListPage.js
+++ src/mypage/trip/TripListPage.js
@@ -7,7 +7,43 @@ import { getUserTripRouteList } from "../../api/myPageApi";
 
 const TripListPage = () => {
   const [curIdx, setCurIdx] = useState(0);
-  const [dataList, setDataList] = useState([]);
+  const [dataList, setDataList] = useState([
+    {
+      content: "빵을 먹어요",
+      coverImageName:
+        process.env.PUBLIC_URL + "/imgs/ganadi.jpg",
+      createdAt: "2025-06-07 00:59:49",
+      creatorId: "qtaro1234",
+      creatorNickName: "qtaro1234",
+      distances: 8434,
+      heartCount: 2,
+      id: 80,
+      imageFile: null,
+      isPrivate: false,
+      locationInfo:
+        "36.45014830556474,126.79537874027916,36.43584941430587,126.78158704803639,36.43555094474115,126.8186144075184,36.463170683562986,126.82399447967995",
+      profileImage:
+        process.env.PUBLIC_URL +"/imgs/ganadi_face.png",
+      title: "가나디와 빵집 투어",
+    },  {
+      content: "빵을 먹어요",
+      coverImageName:
+        process.env.PUBLIC_URL + "/imgs/ganadi.jpg",
+      createdAt: "2025-06-07 00:59:49",
+      creatorId: "qtaro1234",
+      creatorNickName: "qtaro1234",
+      distances: 8434,
+      heartCount: 2,
+      id: 80,
+      imageFile: null,
+      isPrivate: false,
+      locationInfo:
+        "36.45014830556474,126.79537874027916,36.43584941430587,126.78158704803639,36.43555094474115,126.8186144075184,36.463170683562986,126.82399447967995",
+      profileImage:
+        process.env.PUBLIC_URL +"/imgs/ganadi_face.png",
+      title: "가나디와 빵집 투어",
+    },
+  ]);
 
   const getTripRouteList = () => {
     getUserTripRouteList(localStorage.getItem("userToken"))
